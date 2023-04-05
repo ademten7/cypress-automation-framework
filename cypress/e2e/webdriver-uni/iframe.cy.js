552528af6085c256c112fdbfde1a3be06de03a12
ademten7 <ademten7@gmail.com>
2023-04-05 23:23:55 +0200

    added traversing-elments

--- cypress/e2e/webdriver-uni/iframe.cy.js
+++ cypress/e2e/webdriver-uni/iframe.cy.js
@@ -3,12 +3,12 @@ describe("Iframe & Modal Handling  ", () => {
     it("Handling ifarame and Modal", () => {
         cy.visit("http://www.webdriveruniversity.com");
         cy.get('#iframe').invoke('removeAttr','target').click({force:true});
-       
+       //direk olarak iframe icindeki elementlere ulasamam.
         cy.get('#frame').then(($iframe)=>{
             const body = $iframe.contents().find('body');
             cy.wrap(body).as('iframe')
-
         })
+        //Buradan sonra iframe icindeki her elmente ulasabilirim
         cy.get('@iframe').find('#button-find-out-more').click();
         cy.get('@iframe').find('#myModal').as('modal')
         // cy.get('@modal').should(($expectedText)=>{
@@ -17,5 +17,17 @@ describe("Iframe & Modal Handling  ", () => {
         // })
         cy.get('@modal').contains('webdriveruniversity')
         cy.get('@modal').contains('Close').click();
+    });
+    it('Handling ifarame with "cypress-iframe" plugin', () => {
+        cy.visit(url);
+        cy.frameLoaded("#iframeResult");
+    
+        //approach 1 ==> trigger()
+        cy.iframe("#iframeResult")
+          .find("button[class=double-click]")
+          .trigger("dblclick");
+        cy.iframe("#iframeResult")
+          .find("#field2")
+          .should("have.value", "Hello World!");
     });
 });
--- cypress/e2e/webdriver-uni/mouseActions(trigger).cy.js
+++ cypress/e2e/webdriver-uni/mouseActions(trigger).cy.js
@@ -10,11 +10,25 @@ describe("Mouse Actions  ", () => {
 
     });
 
-    it.only("Drag-Drop action", () => { //require("@4tw/cypress-drag-drop"); npm i @4tw/cypress-drag-drop
+    it("Drag-Drop action", () => { //require("@4tw/cypress-drag-drop"); npm i @4tw/cypress-drag-drop
     //  cy.get('#draggable').drag('#droppable',{force:true})
 //OR
         cy.get('#draggable').trigger('mousedown',{which:1}); //'mousedown=left click ,which:1=click center of the elment
         cy.get('#droppable').trigger('mousemove').trigger('mouseup',{force:true})
       
     });
+    it("Scroll double click", () => {
+        cy.get('#double-click').as('double').dblclick();
+        cy.get('@double').should('have.class','double')
+
+
+    });
+    it.only("left mouse Click, hold & assertion", () => {
+      cy.get('#click-box').as('click-box').trigger('mousedown',{which:1}).then(($el)=>{
+        expect($el).to.have.css('background-color', 'rgb(0, 255, 0)')
+        
+
+      })
+    });
+
 });
